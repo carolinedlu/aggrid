8101c027e0a1cb14f47fa50915adf749ff28007e
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-08-31 19:59:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid
 
-df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
-grid_return = AgGrid(df, editable=True)
-new_df = grid_return['data']
+# df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
+# grid_return = AgGrid(df, editable=True)
+# new_df = grid_return['data']
 
 
 def exp_list():
-    st.session_state['exp_list'] = ['2001','2002','2003']
+#     st.session_state['exp_list'] = ['2001','2002','2003']
+    st.write("on change function called")
 
 first_sb = st.selectbox('please select an option', options= ['option1'], key ='first_sb', on_change=exp_list)
 second_sb = st.selectbox('please select a year', options= ['option1','option2'], key ='second_sb')
