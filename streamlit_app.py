d899a31bfd19051fcb789e221d286e304e293ca3
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-09-01 11:34:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,17 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid
 
-# df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
-# grid_return = AgGrid(df, editable=True)
-# new_df = grid_return['data']
+df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
+grid_return = AgGrid(df, editable=True)
+new_df = grid_return['data']
 
 
-def on_change():
-#     st.session_state['exp_list'] = ['2001','2002','2003']
-    st.write("on change function called")
+# def on_change():
+# #     st.session_state['exp_list'] = ['2001','2002','2003']
+#     st.write("on change function called")
 
-first_sb = st.selectbox('please select an option', options= ['option1'], key ='first_sb', on_change=on_change)
-second_sb = st.selectbox('please select a year', options= ['option1','option2'], key ='second_sb')
+# first_sb = st.selectbox('please select an option', options= ['option1'], key ='first_sb', on_change=on_change)
+# second_sb = st.selectbox('please select a year', options= ['option1','option2'], key ='second_sb')
 
-if first_sb:
-    st.write(first_sb)
+# if first_sb:
+#     st.write(first_sb)
